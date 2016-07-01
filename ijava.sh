d2da31de9b7ddd5cda2d9beee09c9194e717d34e
Ƭɧℰ-ℜãψ <jasht.sserie@gmail.com>
2016-07-01 03:29:17 -0700

    Update ijava.sh

--- ijava.sh
+++ ijava.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
 apt-get update; apt-get upgrade -y
-apt-get 
+apt-get install default-jre default-jdk -y
+sudo apt-get install python-software-properties -y
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-get update
+sudo apt-get install oracle-java8-installer -y
+
+#Manual mode.
+sudo update-alternatives --config java
+sudo nano /etc/environment
+JAVA_HOME="path_to_java"
+source /etc/environment
+echo $JAVA_HOME
+export JAVA_HOME=$(/usr/libexec/java_home)
+export PATH=$JAVA_HOME/jre/bin:$PATH
+
+
