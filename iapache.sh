95316a6225ba2dba89f12fc4f924475a34b614e9
Ƭɧℰ-ℜãψ <jasht.sserie@gmail.com>
2016-07-03 16:02:17 +0000

    Update iapache.sh

--- iapache.sh
+++ iapache.sh
@@ -2,18 +2,26 @@
 echo -n "Updating System..."; apt-get update -y > /dev/null; echo "Done.";
 echo -n "Upgrading System..."; apt-get upgrade -y > /dev/null; echo "Done.";
 echo -n "Installing Apache..."; apt-get install apache2 -y > /dev/null; echo "Done.";
+clear
 echo -n "Starting Apache..."; service apache2 start > /dev/null; echo "Done.";
+sleep 1
+clear
 echo -n "Installing Php5..."; apt-get install php5 -y > /dev/null; echo "Done.";
-sleep 0.5
+sleep 2
+clear
 echo -n "Restarting Apache...; service apache2 restart > /dev/null; echo "Done.";
-sleep 0.1
+sleep 1
+clear
 echo -n "Installing Php5 Packs...; apt-get install -y libapache2-mod-php5 php5-mysql php5-curl php5-gd php5-intl php-pear php5-imagick php5-imap php5-mcrypt php5-ming php5-ps > /dev/null; echo "Done.";
 echo -n "Installing Php5 Packs...; apt-get install -y php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl > /dev/null; echo "Done.";
 echo -n "Restarting Apache...; service apache2 restart > /dev/null; echo "Done.";
-sleep 0.3
+sleep 1
+clear
 echo -n "Enable Php5EnMod...; sudo php5enmod mcrypt > /dev/null; echo "Done.";
 echo -n "Enable Mod_Rewrite...; sudo a2enmod rewrite > /dev/null; echo "Done.";
-sleep 0.3
+sleep 1
+clear
 echo -n "Restarting Apache...; service apache2 restart > /dev/null; echo "Done.";
+clear
 echo -n "Work Done.; rm -Rf ~/iapache.sh > /dev/null; echo "BYE!";
-sleep 0.5
+sleep 1
