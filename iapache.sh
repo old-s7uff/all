f1f593531efdc1ebf0a701326692e8c4eb7835f5
Ƭɧℰ-ℜãψ <jasht.sserie@gmail.com>
2016-07-04 01:49:04 -0700

    Update iapache.sh

--- iapache.sh
+++ iapache.sh
@@ -12,12 +12,14 @@ clear
 echo -n "Restarting Apache...; service apache2 restart > /dev/null; echo "Done.";
 sleep 1
 clear
-echo -n "Installing Php5 Packs...; apt-get install -y libapache2-mod-php5 php5-mysql php5-curl php5-gd php5-intl php-pear php5-imagick php5-imap php5-mcrypt php5-ming php5-ps > /dev/null; echo "Done.";
-echo -n "Installing Php5 Packs...; apt-get install -y php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl > /dev/null; echo "Done.";
-echo -n "Restarting Apache...; service apache2 restart > /dev/null; echo "Done.";
+apt-get install -y libapache2-mod-php5 php5-mysql php5-curl php5-gd php5-intl php-pear php5-imagick php5-imap php5-mcrypt php5-ming php5-ps
+apt-get install -y php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl
+service apache2 restart
 sleep 1
 clear
 echo -n "Enable Php5EnMod...; sudo php5enmod mcrypt > /dev/null; echo "Done.";
+sleep 1
+clear
 echo -n "Enable Mod_Rewrite...; sudo a2enmod rewrite > /dev/null; echo "Done.";
 sleep 1
 clear
