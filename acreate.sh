ca4f3bfc868b70a1ccc13ab85dc710eb28b907f3
Ƭɧℰ-ℜãψ <jasht.sserie@gmail.com>
2016-07-06 07:29:35 +0000

    Update acreate.sh

--- acreate.sh
+++ acreate.sh
@@ -8,7 +8,10 @@ then
 mkdir -p $vf/$1/$p
 cat <<EOF > $vf/$1/$p/$i
 <html>
-
+<h1><center>DEFAULT INDEX.</h1></center>
+<h1><center>Thanks By Using RAW's Project.</h1></center>
+<h2><center>Please Delete This Index At /hostdata/$1/$p/$i</h2></center>
+<h2><center>And Upload U'r Files At /hostdata/$1/$p
 </html>
 EOF
 
