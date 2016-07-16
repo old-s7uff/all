7e6bd255349ef12ef2548ad399be0029474f6f90
Ƭɧℰ-ℜãψ <jasht.sserie@gmail.com>
2016-07-16 10:43:39 +0000

    Create gnome-core.sh

---
+++ gnome-core.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+apt-get update -y 
+apt-get upgrade -y
+apt-get dist-upgrade -y
+sudo apt-get install gnome-core -y
+sudo apt-get install gnome-desktop-environment -y
+apt-get install synaptic -y
+apt-get install gparted -y
