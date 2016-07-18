9b9299607585f2554ea3ddcb8175b84d4d1f537f
Ƭɧℰ-ℜãψ <jasht.sserie@gmail.com>
2016-07-18 12:54:56 -0700

    Update inode.sh

--- inode.sh
+++ inode.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
-apt-get update; apt-get upgrade -y
-apt-get install git libssl-dev openssl nano wget curl build-essential -y
-curl -sL https://deb.nodesource.com/setup_4.x | sudo bash -
-apt-get install nodejs -y
-npm cache clean -f
-npm install -g n
-n 6.0.0
-sudo ln -sf /usr/local/n/versions/node/6.0.0/bin/node /usr/bin/node
+echo -n "Updating System..."; apt-get update -y  > /dev/null; echo "Done.";
+echo -n "Upgrading System..."; apt-get upgrade -y  > /dev/null; echo "Done.";
+echo -n "Installing Packs..."; apt-get install -y git libssl-dev openssl nano wget curl > /dev/null; echo "Done.";
+echo -n "Installing Packs..."; apt-get install dnsutils build-essential -y  > /dev/null; echo "Done.";
+echo -n "Getting Node Source..."; curl -sL https://deb.nodesource.com/setup_4.x | sudo bash -  > /dev/null; echo "Done.";
+echo -n "Installing Node..."; apt-get install nodejs -y > /dev/null; echo "Done.";
+echo -n "Clear Cache..."; npm cache clean -f  > /dev/null; echo "Done.";
+echo -n "Get Node..."; npm install -g n  > /dev/null; echo "Done.";
+echo -n "Install Node..."; n 5.9.0  > /dev/null; echo "Done.";
+echo -n "Creating A Symlink..."; sudo ln -sf /usr/local/n/versions/node/5.9.0/bin/node /usr/bin/node > /dev/null; echo "Done.";
+echo -n "Fixing Node Prefix..."; npm config set prefix /usr/local > /dev/null; echo "Done.";
+echo "*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*"
+echo "*-*-*-*-*--Node echo "`node -v`" Installed.--*-*-*-*-**"
+echo "*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*"
