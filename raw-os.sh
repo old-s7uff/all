cace4f84d58f0c69a39ed3191fe18f0190d6661a
Ƭɧℰ-ℜãψ <jasht.sserie@gmail.com>
2016-07-18 17:33:59 +0000

    Update raw-os.sh

--- raw-os.sh
+++ raw-os.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+clear
+*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-* *-**-*-*-*-*-*-*-
+
 apt-get update -y
 apt-get upgrade -y
 apt-get dist-upgrade -y
@@ -15,6 +18,29 @@ sudo add-apt-repository ppa:upubuntu-com/icons -y
 sudo apt-get update -y
 sudo apt-get install numix-bluish-theme -y
 sudo apt-get install superflat-icon-theme -y
+
+#Text dev.
+cd ~/work
+mkdir 1/
+cd 1/
+
+cd ~/
+rm -Rf ~/work/1/
+
+
+
+mkdir sudo/
+cd sudo/
+apt-get install git nmap nano mate-common yelp-tools -y
+git clone https://github.com/mate-desktop/pluma.git
+cd pluma/
+sudo ./autogen.sh
+sudo make
+sudo make install
+cd ~/
+rm -Rf ~/work/sudo/
+
+
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - -y
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 sudo apt-get update -y
