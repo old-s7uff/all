8ed82915e122a3b006eb5a246b5f5aa91dc9a61d
Ƭɧℰ-ℜãψ <jasht.sserie@gmail.com>
2016-07-18 17:11:48 +0000

    Update raw-os.sh

--- raw-os.sh
+++ raw-os.sh
@@ -1,3 +1,22 @@
 #!/bin/bash
 apt-get update -y
 apt-get upgrade -y
+apt-get dist-upgrade -y
+sudo apt-get install gnome-core -y
+sudo apt-get install gnome-desktop-environment -y
+apt-get install synaptic -y
+apt-get install gparted -y
+mkdir ~/work
+cd ~/work
+wget https://github.com/adobe/brackets/releases/download/release-1.7/Brackets.Release.1.7.64-bit.deb
+dpkg -i Brackets.Release.1.7.64-bit.deb
+sudo add-apt-repository ppa:noobslab/themes -y
+sudo add-apt-repository ppa:upubuntu-com/icons -y
+sudo apt-get update -y
+sudo apt-get install numix-bluish-theme -y
+sudo apt-get install superflat-icon-theme -y
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - -y
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+sudo apt-get update -y
+sudo apt-get install google-chrome-stable -y
+rm -Rf ~/work
