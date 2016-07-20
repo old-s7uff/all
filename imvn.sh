d19f88ce1c2e645f7b67fc7a7f253c11005f50e1
Ƭɧℰ-ℜãψ <jasht.sserie@gmail.com>
2016-07-20 22:41:02 +0000

    Update imvn.sh

--- imvn.sh
+++ imvn.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo add-apt-repository ppa:andrei-pozolotin/maven3
-sudo apt-get update
+sudo add-apt-repository ppa:andrei-pozolotin/maven3 -y
+sudo apt-get update -y
 sudo apt-get install maven3 -y
-sudo ln -s /usr/share/maven3/bin/mvn /usr/bin/mvn
+
