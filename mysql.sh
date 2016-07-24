2a89b100b325421e7add43c676bf9e5373a617e0
Ƭɧℰ-ℜãψ <jasht.sserie@gmail.com>
2016-07-24 16:13:59 +0000

    Create mysql.sh

---
+++ mysql.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+apt-get update
+apt-get install expect -y
+
+VAR=$(expect -c '
+spawn apt-get install mysql-server -y
+expect "New password for the MySQL \"root\" user:"
+send "root\r"
+expect "Repeat password for the MySQL \"root\" user:"
+send "root\r"
+expect eof
+')
+
+echo "$VAR"
+
+apt-get install mysql-client libmysqlclient15-dev -y
+clear
+/etc/init.d/mysql stop
+/etc/init.d/mysql start
+sleep 2
+clear
