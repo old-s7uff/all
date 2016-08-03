5c3828eb2c581149e2f6427fd4b98b3d62d0cbf0
Ƭɧℰ-ℜãψ <jasht.sserie@gmail.com>
2016-08-03 23:11:40 +0000

    Create cs.sh

---
+++ cs.sh
@@ -0,0 +1,5 @@
+yum install glibc.i686 screen nano unzip ld-linux.so.2 ncompress -y
+HLDS PUBLIC
+wget decani-gaming.al/k1ll3riihlds/public.zip
+HLDS CLANWAR
+wget decani-gaming.al/k1ll3riihlds/clanwar.zip
