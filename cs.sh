32a79bc460c2ce8887224738e2391489581392a2
Ƭɧℰ-ℜãψ <jasht.sserie@gmail.com>
2016-08-04 00:21:14 +0000

    Update cs.sh

--- cs.sh
+++ cs.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 if [ "`lsb_release -is`" == "CentOS" ]
+#CentOS
 then
    mkdir -p /servers/cs
    yum -y install glibc.i686 screen nano unzip ld-linux.so.2 ncompress
    ds='/servers/cs'
    cd $ds
-   https://cloud.dope-us.com/public.zip
+   wget https://cloud.dope-us.com/public.zip
    unzip public.zip
    rm -Rf public.zip
    chmod +x hlds_run
-fi
+   
+#Ubuntu
+elif [ "`lsb_release -is`" == "Ubuntu" ]
+    mkdir -p /servers/cs
+    ds='/servers/cs'
+    cd $ds
+    wget https://cloud.dope-us.com/public.zip
+    unzip public.zip
+    rm -Rf public.zip
+    chmod +x hlds_run
+    
+#END
 else
     echo "Your System Isn't Supported Sorry."
 fi
