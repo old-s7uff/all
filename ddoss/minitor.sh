5181e2f969c7eb330023efaa43451da430c7474f
Ƭɧℰ-ℜãψ <jasht.sserie@gmail.com>
2016-08-25 18:22:06 +0000

    Update minitor.sh

--- ddoss/minitor.sh
+++ ddoss/minitor.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 if [[ "$@" == 'm 21' ]]; 
 then
-tcpdump port $2 and '(tcp-syn|tcp-ack)!=0' > log21.txt
+tcpdump -i any port 21 > log21.txt
 fi
 if [[ "$@" == 'm 22' ]]; 
 then
-tcpdump port $2 and '(tcp-syn|tcp-ack)!=0' > log22.txt
+tcpdump -i any port 22 > log22.txt
 fi
 if [[ "$@" == 'm 25' ]]; 
 then
-tcpdump port $2 and '(tcp-syn|tcp-ack)!=0' > log25.txt
+tcpdump -i any port 25 > log25.txt
 fi
 if [[ "$@" == 'm 53' ]]; 
 then
-tcpdump port $2 and '(tcp-syn|tcp-ack)!=0' > log53.txt
+tcpdump -i any port 53 > log53.txt
 fi
 if [[ "$@" == 'm 80' ]]; 
 then
-tcpdump port $2 and '(tcp-syn|tcp-ack)!=0' > log80.txt
+tcpdump -i any port 80 > log80.txt
 fi
 if [[ "$@" == 'm 443' ]]; 
 then
-tcpdump port $2 and '(tcp-syn|tcp-ack)!=0' > log443.txt
+tcpdump -i any port 443 > log443.txt
 fi
 if [[ "$@" == 'm 27015' ]]; 
 then
-tcpdump port $2 and '(tcp-syn|tcp-ack)!=0' > log27015.txt
+tcpdump -i any port 27015 > log27015.txt
 fi
