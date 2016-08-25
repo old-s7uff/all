dfe4408e577aed1283cfe75c2ba3a542ad975741
Ƭɧℰ-ℜãψ <jasht.sserie@gmail.com>
2016-08-25 18:29:24 +0000

    Update monitor.sh

--- ddos/monitor.sh
+++ ddos/monitor.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
-if [[ "$@" == 'm 21' ]]; 
+if [[ "$1" == '21' ]]; 
 then
 tcpdump -i any port 21 > log21.txt
 fi
-if [[ "$@" == 'm 22' ]]; 
+if [[ "$1" == '22' ]]; 
 then
 tcpdump -i any port 22 > log22.txt
 fi
-if [[ "$@" == 'm 25' ]]; 
+if [[ "$1" == '25' ]]; 
 then
 tcpdump -i any port 25 > log25.txt
 fi
-if [[ "$@" == 'm 53' ]]; 
+if [[ "$1" == '53' ]]; 
 then
 tcpdump -i any port 53 > log53.txt
 fi
-if [[ "$@" == 'm 80' ]]; 
+if [[ "$1" == '80' ]]; 
 then
 tcpdump -i any port 80 > log80.txt
 fi
-if [[ "$@" == 'm 443' ]]; 
+if [[ "$1" == '443' ]]; 
 then
 tcpdump -i any port 443 > log443.txt
 fi
-if [[ "$@" == 'm 27015' ]]; 
+if [[ "$1" == '27015' ]]; 
 then
 tcpdump -i any port 27015 > log27015.txt
 fi
