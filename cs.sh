be11e912b81cbaf9d139ed04c3b7e9a13614cbca
ƬHE ЯAW  ☣ <jasht.sserie@gmail.com>
2016-11-06 22:22:24 +0000

    Update cs.sh

--- cs.sh
+++ cs.sh
@@ -2,11 +2,13 @@
 if [ "`lsb_release -is`" == "CentOS" ]
 #CentOS
 then
+   yum update; yum upgrade
+   yum install libssl-dev epel-release -y
    mkdir -p /servers/cs
    yum -y install glibc.i686 screen nano unzip ld-linux.so.2 ncompress
    ds='/servers/cs'
    cd $ds
-   wget https://cloud.dope-us.com/public.zip
+   wget http://cloud.dope-us.com/public.zip
    unzip public.zip
    rm -Rf public.zip
    chmod +x hlds_run
@@ -15,12 +17,13 @@ elif [ "`lsb_release -is`" == "Ubuntu" ] || [ "`lsb_release -is`" == "Debian" ]
 #Ubuntu
 then
     apt-get install ia32-libs -y
+    apt-get install git screen zip unzip libssl-dev build-essential -y
     apt-get install lib32ncurses5 lib32z1 -y
     apt-get install lib32z1 lib32ncurses5 lib32bz2-1.0 -y
     mkdir -p /servers/cs
     ds='/servers/cs'
     cd $ds
-    wget https://cloud.dope-us.com/public.zip
+    wget http://cloud.dope-us.com/public.zip
     unzip public.zip
     rm -Rf public.zip
     chmod +x hlds_run
