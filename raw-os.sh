794385e211dd5fd69c7938463b2d5e211caccff4
ƬHE ЯAW  ☣ <jasht.sserie@gmail.com>
2016-11-13 09:53:39 +0000

    Update raw-os.sh

--- raw-os.sh
+++ raw-os.sh
@@ -15,6 +15,7 @@ apt-get upgrade -y
 apt-get dist-upgrade -y
 sudo apt-get install gnome-core -y
 sudo apt-get install gnome-desktop-environment -y
+sudo apt-get install gnome-system-tools -y
 apt-get install synaptic -y
 apt-get install gparted -y
 
