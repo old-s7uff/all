5c71b4849dc252c5ecc59ee1388caaac07708625
ƬHE ЯAW  ☣ <jasht.sserie@gmail.com>
2017-01-20 17:45:21 +0100

    Create cls1.sh

---
+++ cls1.sh
@@ -0,0 +1,96 @@
+#!/bin/bash
+rm -Rf /opt/steamcmd/
+case "`lsb_release -is`" in
+     Ubuntu)
+             apt-get update; apt-get upgrade -y
+             apt-get install lib32z1 lib32ncurses5 lib32bz2-1.0 lib32gcc1 screen wget git curl -y
+             apt-get install ia32-libs -y
+             cd /opt
+             mkdir steamcmd && cd steamcmd/
+             clear
+             curl -sqL 'https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz' | tar zxvf -
+             ./steamcmd.sh +quit
+             mkdir /usr/getit
+             cd /usr/getit
+             wget https://gist.githubusercontent.com/systemroot/796288a279f898834b8bad4d02d7e924/raw/7152b8387f302272d2d3e2db2c61337b78425d9e/f1.sh
+             wget https://gist.githubusercontent.com/systemroot/40685f26f4c94bf60d43e5c50a9c3a5f/raw/02d60df15af6ea5f538b9ee1dc179748c3a517c9/s1.sh
+             wget https://gist.githubusercontent.com/systemroot/cb63951e279565303f0e5883fbde682e/raw/bf7e5342131deac21a486200fc9d5af5ec14b865/f3.sh
+             wget https://gist.githubusercontent.com/systemroot/9bca09a6ee3afb08048d4e3aca3d516a/raw/72828ee458f571b13ccc93e8b6d4d57174384a64/f4.sh
+             chmod +x *
+             ./f1.sh
+             ./s1.sh
+             ./f3.sh
+             ./f4.sh
+             rm -Rf /usr/getit
+             apt-get install apache2 -y
+             apt-get install php5 -y
+             apt-get install -y libapache2-mod-php5 php5-mysql php5-curl php5-gd php5-intl php-pear php5-imagick php5-imap php5-mcrypt php5-ming php5-ps
+             apt-get install -y php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl
+             service nginx stop
+             service apache2 start
+             mkdir -p /var/www/html/modules/servers/licensing/
+             cd /var/www/html/modules/servers/licensing/
+             wget https://gist.githubusercontent.com/systemroot/6cd55bdea3f5e1668f4d8c299f667121/raw/386ae284395f15abc4a6d91217d23e171531e90f/remote.php
+             chmod -R 777 /var/www/html/
+             chmod -R 777 /var/www/html/modules
+             chmod -R 777 /var/www/html/modules/servers
+             chmod -R 777 /var/www/html/modules/servers/licensing
+             chmod -R 777 /var/www/html/modules/servers/licensing/remote.php
+             chmod +x ~/gcp.sh
+             cd ~/
+             ./gcp.sh
+             echo "Done"
+             rm -Rf ~/cls.sh
+             rm -Rf ~/gcp.sh
+     ;;
+     CentOS)
+             yum -y update
+             yum -y upgrade
+             yum -y install epel-release
+             yum -y install glibc libstdc++ screen nano wget curl
+             yum -y install glibc.i686 libgcc.x86_64 libgcc_s.so.1
+             cd /opt
+             mkdir steamcmd
+             cd steamcmd/
+             curl -sqL 'https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz' | tar zxvf -
+             clear
+             ./steamcmd.sh +quit
+             clear
+             yum -y install httpd
+             yum -y install php
+             yum -y install php-gd php-ldap php-odbc php-pear php-xml php-xmlrpc php-mbstring php-snmp php-soap curl curl-devel
+             clear
+             systemctl start httpd.service
+             systemctl enable httpd.service
+             service httpd stop
+             service httpd start
+             mkdir -p /var/www/html/modules/servers/licensing/
+             cd /var/www/html/modules/servers/licensing/
+             wget https://gist.githubusercontent.com/systemroot/6cd55bdea3f5e1668f4d8c299f667121/raw/386ae284395f15abc4a6d91217d23e171531e90f/remote.php
+             chmod -R 777 /var/www/html/
+             chmod -R 777 /var/www/html/modules
+             chmod -R 777 /var/www/html/modules/servers
+             chmod -R 777 /var/www/html/modules/servers/licensing
+             chmod -R 777 /var/www/html/modules/servers/licensing/remote.php
+             clear
+             mkdir /usr/getit
+             cd /usr/getit
+             wget https://gist.githubusercontent.com/systemroot/796288a279f898834b8bad4d02d7e924/raw/7152b8387f302272d2d3e2db2c61337b78425d9e/f1.sh
+             wget https://gist.githubusercontent.com/systemroot/40685f26f4c94bf60d43e5c50a9c3a5f/raw/02d60df15af6ea5f538b9ee1dc179748c3a517c9/s1.sh
+             wget https://gist.githubusercontent.com/systemroot/cb63951e279565303f0e5883fbde682e/raw/bf7e5342131deac21a486200fc9d5af5ec14b865/f3.sh
+             wget https://gist.githubusercontent.com/systemroot/9bca09a6ee3afb08048d4e3aca3d516a/raw/72828ee458f571b13ccc93e8b6d4d57174384a64/f4.sh
+             chmod +x *
+             ./f1.sh
+             ./s1.sh
+             ./f3.sh
+             ./f4.sh
+             rm -Rf /usr/getit
+             service httpd restart
+             cd ~/
+             chmod +x ~/gcp.sh
+             ./gcp.sh
+             echo "Done."
+             rm -Rf ~/cls.sh
+             rm -Rf ~/gcp.sh
+     ;;
+esac
