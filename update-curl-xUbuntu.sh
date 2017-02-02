391da87af3130c0e2b78817317c4ac5ef575efb8
ƬHE ЯAW  ☣ <jasht.sserie@gmail.com>
2017-02-02 09:04:30 +0000

    Create update-curl-xUbuntu.sh

---
+++ update-curl-xUbuntu.sh
@@ -0,0 +1,16 @@
+apt-get update; apt-get upgrade -y; apt-get dist-upgrade -y; apt-get install binutils -y
+mkdir -p /opt/curl
+cd /opt/curl
+wget http://curl.haxx.se/download/curl-7.50.2.tar.bz2
+tar -xvjf curl-7.50.2.tar.bz2
+cd curl-7.50.2
+./configure
+make
+sudo make install
+sudo ldconfig
+
+# i know. you will still have old curl so
+apt-get remove curl -y
+cp /usr/local/bin/curl /bin/curl
+
+#Now 'sudo -i' and 'curl -V'
