7b7c0e03f028a9c9d920e1bfe8e0f63f18b8a240
ƬHE ЯAW  ☣ <jasht.sserie@gmail.com>
2017-10-09 17:30:35 +0000

    Delete update.sh

--- update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd ~/
-rm -Rf .raws
-git clone https://github.com/systemroot/raws.git &>/dev/null
-mv raws/.raws ~/ && rm -Rf ~/raws
-chmod +x .raws/* .raws/.*
-clear
-echo " Update Is Complete !"
-rm -Rf ~/update.sh
